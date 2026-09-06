6d4af8794dfdef4dc798a106cddc1ba063928a64
caner-soyer <canersoyer@gmail.com>
2026-09-06 14:36:29 +0100

    Add raster-aware warnings and image editor tabs

--- app/editor/layout.tsx
+++ app/editor/layout.tsx
@@ -23,6 +23,7 @@ import "./v61.css";
 import "./v62.css";
 import "./v63.css";
 import "./v64.css";
+import "./v65.css";
 
 export default function EditorLayout({ children }: { children: React.ReactNode }) {
   const router = useRouter();
